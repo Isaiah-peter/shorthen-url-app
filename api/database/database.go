fa546c7e3419b847e8b2ee76d4ca36952241c41e
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-02-06 17:36:28 +0100

    more

--- api/database/database.go
+++ api/database/database.go
@@ -7,11 +7,14 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
- var ctx = context.Background()
- func CreateClient(dbbase int) *redis.Client{
-	 rdb := redis.NewClient(&redis.Options{
-		 Addr: os.Getenv("") ,
-		 Password: ,
-		 DB: dbdbbase,
-	 })
- }
+var ctx = context.Background()
+
+func CreateClient(dbbase int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB:       dbbase,
+	})
+
+	return rdb
+}
