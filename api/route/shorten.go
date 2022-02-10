e92d9eadf6d0df631f0b9437efa1c57cf941c7d1
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-02-10 05:14:08 +0100

    more

--- api/route/shorten.go
+++ api/route/shorten.go
@@ -1,10 +1,15 @@
 package route
 
 import (
+	"context"
+	"os"
+	"strconv"
 	"time"
 
+	"shorten-url-with-redis/database"
 	"shorten-url-with-redis/helper"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -23,11 +28,27 @@ type response struct {
 }
 
 func ShortenUrl(c *fiber.Ctx) error {
+	ctx := context.Background()
 	body := new(request)
 	if err := c.BodyParser(&body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
 	}
 	//implement rate limiting
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	value, err := r2.Get(ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(ctx, c.IP(), os.Getenv("API_QOUTA"), 30*60*time.Second)
+	} else {
+		value, _ := r2.Get(database.Ctx, c.IP()).Result()
+		valueInt, _ := strconv.Atoi(value)
+		if valueInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "rate exceeded", "rate_limit_rest": limit / time.Nanosecond / time.Minute})
+		}
+
+	}
 
 	//check if the input sent is a URL
 	if !govalidator.isURL(body.URL) {
@@ -41,5 +62,5 @@ func ShortenUrl(c *fiber.Ctx) error {
 	//enforce  http SSL
 
 	body.URL = helper.EnforceHTTP(body.URL)
-
+	r2.Decr(database.Ctx, c.IP())
 }
